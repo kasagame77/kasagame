aebaee3e6dd607156377228f86b8329933a3473b
Kaptan SA <kaptan@kasagame.com>
2024-03-26 00:32:03 +0300

    django authentication

--- kasagame/settings.py
+++ kasagame/settings.py
@@ -138,3 +138,18 @@ MEDIA_ROOT = BASE_DIR /'media'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: "danger",
+}
+
+# SMTP configiration
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'kasagamex@gmail.com'
+EMAIL_HOST_PASSWORD = 'uldx jygm urud eohh'
+EMAIL_USE_TLS = True
